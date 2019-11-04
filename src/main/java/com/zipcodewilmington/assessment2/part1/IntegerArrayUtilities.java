68dc65f58d6b05a3468a898f1b82e16102ff2864
mike <mike.schoenbeck@gmail.com>
2019-11-04 10:21:38 -0500

    now really completed intArrayUtil

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -15,10 +15,11 @@ public class IntegerArrayUtilities {
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+
+        return array[0] + array[1];
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
-        return null;
+        return array[array.length-1] * array[array.length - 2];
     }
 }
