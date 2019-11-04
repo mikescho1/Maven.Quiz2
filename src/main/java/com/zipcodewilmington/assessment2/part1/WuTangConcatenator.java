d1b586a7493cd9f6c850ad99054c5d1457dd8f73
mike <mike.schoenbeck@gmail.com>
2019-11-04 14:10:45 -0500

    completed rotate array...wtf

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,18 +1,32 @@
 package com.zipcodewilmington.assessment2.part1;
 
+
+import java.util.logging.Logger;
+
 public class WuTangConcatenator {
+    private Integer input;
+    private String name;
+
+    private static Logger logger = Logger.getGlobal();
+
+
     public WuTangConcatenator(Integer input) {
+        this.input = input;
+
+
     }
 
     public Boolean isWu() {
-        return null;
+        return this.input % 3 == 0;
     }
 
+
     public Boolean isTang() {
-        return null;
+        return this.input % 5 == 0;
     }
 
     public Boolean isWuTang() {
-        return null;
+        return isWu() && isTang();
     }
 }
+
--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,13 +1,31 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayUtility {
     public Integer[] merge(Integer[] array1, Integer[] array2) {
         return null;
     }
 
     public Integer[] rotate(Integer[] array, Integer index) {
-        return null;
+        Integer[] newArr = new Integer[array.length];
+
+
+        for (int i = 0; i < array.length; i++) {
+            if (i > index - 1) {
+                newArr[i - index] = array[i];
+            }   if(i < index)   {
+                newArr[array.length - (index - i)] = array[i];
+            }
+        }
+
+        return newArr;
     }
+
+
 
     public Integer countOccurrence(Integer[] array1, Integer[] array2, Integer valueToEvaluate) {
         return null;
