62415608e1fe08d41b11c8b1db6517e91b9ee771
mike <mike.schoenbeck@gmail.com>
2019-11-04 09:54:33 -0500

    completed basic utilities

--- src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/BasicUtilities.java
@@ -2,18 +2,26 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class BasicUtilities {
     public Boolean isGreaterThan5(Integer value) {
-        return null;
+        return (value >= 5) ? true : false;
     }
 
     public Boolean isLessThan7(Integer value) {
-        return null;
+        return (value <= 7) ? true: false;
     }
 
     public Boolean isBetween5And7(Integer valueToEvaluate) {
-        return null;
+        return isLessThan7(valueToEvaluate) && isGreaterThan5(valueToEvaluate);
     }
 
     public Boolean startsWith(String string, Character character) {
-        return null;
+
+        boolean startsWith1 = (string.charAt(0) == Character.toLowerCase(character));
+        boolean startsWith2 = (string.charAt(0) == Character.toUpperCase(character));
+
+
+
+        return startsWith1 || startsWith2;
     }
+
+
 }
